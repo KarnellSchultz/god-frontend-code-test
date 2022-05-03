2573e4ec637eeea53504286780127e1e728edb14
karnell schultz <50870173+KarnellSchultz@users.noreply.github.com>
2022-05-03 20:55:28 +0200

    created layout

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,15 @@
 import React, { useEffect } from "react";
-
 import Link from "next/link";
 import Image from "next/image";
 
-import { getCars } from "./api/cars";
-import { CarType } from "../src/types";
-import { Layout } from "../src/components/layout";
-import { Card, CardContent, Spacer, Text, Link as VLink, Icon } from "vcc-ui";
-
 import styled from "styled-components";
+
+import { Spacer, Text, Link as VLink, Icon } from "vcc-ui";
+
+import { CarType } from "../src/types";
+import { getCars } from "./api/cars";
+import { Layout } from "../src/components/layout";
+import { ArrowButtons } from "../src/components/ArrowButtons";
 
 const CarsContainer = styled.div`
   display: grid;
@@ -24,15 +25,9 @@ const LinkContainer = styled.div`
   justify-content: space-evenly;
 `;
 
-const ArrowButton = styled.div`
-  :hover {
-    cursor: pointer;
-  }
-`;
-
-const ButtonContainer = styled.div`
-  display: flex;
-  justify-content: flex-end;
+const CardHeading = styled.div`
+  padding: 0px;
+  paddingbottom: "1rem";
 `;
 
 const App = () => {
@@ -43,57 +38,43 @@ const App = () => {
       .then((data) => setCars(data));
   }, []);
 
+
+
   return (
     <Layout>
       <CarsContainer>
         {cars?.map((car: CarType) => (
           <CarCard key={car.id}>
-            <Card
-              style={{
-                width: "384px",
-                height: "388px",
-                borderRadius: "0px",
-                boxShadow: "none",
-              }}
-            >
-              <CardContent style={{ padding: "0px", paddingBottom: "1rem" }}>
-                <Text variant="columbus" subStyle={"inline-link"}>
-                  {car.bodyType}
+            <CardHeading>
+              <Text variant="columbus" subStyle={"inline-link"}>
+                {car.bodyType}
+              </Text>
+              <div style={{ display: "flex" }}>
+                <Text variant={"amundsen"} subStyle="emphasis">
+                  {car.modelName}
                 </Text>
-                <div style={{ display: "flex" }}>
-                  <Text variant={"amundsen"} subStyle="emphasis">
-                    {car.modelName}
-                  </Text>
-                  <Text subStyle={"inline-link"}>{car.modelType}</Text>
-                </div>
-              </CardContent>
-              <Image
-                src={car.imageUrl}
-                alt="cool car stuff"
-                width={250}
-                height={400}
-              />
-              <LinkContainer>
-                <Link href={`/learn/${car.id}`} passHref>
-                  <VLink arrow="right">Learn</VLink>
-                </Link>
-                <Link href={`/shop/${car.id}`} passHref>
-                  <VLink arrow="right">Shop</VLink>
-                </Link>
-              </LinkContainer>
-            </Card>
+                <Text subStyle={"inline-link"}>{car.modelType}</Text>
+              </div>
+            </CardHeading>
+            <Image
+              src={car.imageUrl}
+              alt="cool car stuff"
+              width={256}
+              height={192}
+            />
+            <LinkContainer>
+              <Link href={`/learn/${car.id}`} passHref>
+                <VLink arrow="right">Learn</VLink>
+              </Link>
+              <Link href={`/shop/${car.id}`} passHref>
+                <VLink arrow="right">Shop</VLink>
+              </Link>
+            </LinkContainer>
           </CarCard>
         ))}
       </CarsContainer>
 
-      <ButtonContainer>
-        <ArrowButton>
-          <Icon type="media-arrowleft-40" />
-        </ArrowButton>
-        <ArrowButton>
-          <Icon type="media-next-40" />
-        </ArrowButton>
-      </ButtonContainer>
+      <ArrowButtons />
     </Layout>
   );
 };
