98943cd843587e8bc28691e791c9b5321ac7d601
Karnell Schultz <50870173+KarnellSchultz@users.noreply.github.com>
2023-03-14 14:28:04 +0100

    fix vercel builds

--- src/utils.tsx
+++ src/utils.tsx
@@ -4,9 +4,10 @@ import { CarType } from './types'
 // API calls
 
 export const ORIGIN_URL =
-    process.env.NODE_ENV === 'development'
-        ? process.env.localhost
-        : `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`
+    process.env.NEXT_PUBLIC_ORIGIN_URL ??
+    (process.env.NEXT_PUBLIC_VERCEL_URL
+        ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`
+        : 'http://localhost:3000')
 
 const routes = {
     cars: ORIGIN_URL + '/api/cars',
